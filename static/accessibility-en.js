3e3dfedc2ecf9ebe1975cec10e7ceb1890a09ba6
edcous <49954272+edcous@users.noreply.github.com>
2021-03-09 13:35:29 -0600

    add accessibility, and other info

---
+++ static/accessibility-en.js
@@ -0,0 +1,30 @@
+onLoadRestore()
+function onLoadRestore(){
+    const fontSize = localStorage.getItem("textsize");
+    $("#all").css("fontSize", fontSize);
+    const contrast = localStorage.getItem("inverted")
+    $("#all").css("filter", contrast);
+}
+function increaseText() {
+    var fontSize = $("#all").css("fontSize");
+    fontSize = parseInt(fontSize) + 2 + "px";
+    $("#all").css("fontSize", fontSize);
+    localStorage.setItem("textsize", fontSize)
+}
+function decreaseText() {
+    var fontSize = $("#all").css("fontSize");
+    fontSize = parseInt(fontSize) - 2 + "px";
+    $("#all").css("fontSize", fontSize);
+    localStorage.setItem("textsize", fontSize)
+}
+function enableHighContrast() {
+    var contrast = $("#all").css("filter");
+    if(contrast != "invert(1)"){
+        $("#all").css("filter", "invert(1)");
+        localStorage.setItem("inverted", "invert(1)")
+    }
+    else{
+        $("#all").css("filter", "");
+        localStorage.setItem("inverted", "")
+    }
+}
